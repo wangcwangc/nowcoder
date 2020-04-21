9ffa2bad019b11ae3c4d9ac27ec7bcff5d5ac7d8
wangcwangc <858256500@qq.com>
2020-04-21 20:42:38 +0800

    用两个栈实现队列

---
+++ src/main/java/剑指offer/用两个栈实现队列.java
@@ -0,0 +1,23 @@
+package 剑指offer;
+
+import java.util.Stack;
+
+public class 用两个栈实现队列 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int result = stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return result;
+    }
+}
