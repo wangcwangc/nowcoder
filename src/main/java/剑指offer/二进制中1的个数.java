c6fd75249576a48319436bd027265df1bae51877
wangcwangc <858256500@qq.com>
2020-04-22 15:33:33 +0800

    二进制中1的个数

---
+++ src/main/java/剑指offer/二进制中1的个数.java
@@ -0,0 +1,12 @@
+package 剑指offer;
+
+public class 二进制中1的个数 {
+    public int NumberOf1(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+}
