a3ed9b7314548ea2e3e17c0bc3d7c27d9c9de857
wangcwangc <858256500@qq.com>
2020-04-23 15:07:42 +0800

    合并两个排序的链表

---
+++ src/main/java/剑指offer/合并两个排序的链表.java
@@ -0,0 +1,15 @@
+package 剑指offer;
+
+public class 合并两个排序的链表 {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val <= list2.val) {
+            list1.next = Merge(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = Merge(list1, list2.next);
+            return list2;
+        }
+    }
+}
